cb5ea2c9abe1c8babf95fbf60ef4937fa5e0464b
Ghazi Sultan <ghazisultan25@gmail.com>
2022-05-08 15:35:41 +0530

    remove secret

--- django_youtube/settings.py
+++ django_youtube/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o^0lhozerin#v#yhoyxangxcayfth_^oh6i*vt8%z1l-9@(_vp'
+SECRET_KEY = 'XXXX'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
